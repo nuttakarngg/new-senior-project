a3dc5b0038aacf467ccffad27e7a47cf9f56764d
TtChaiyapol <zemorez@gmail.com>
2021-11-25 14:28:49 +0700

    update

--- routes/Research.js
+++ routes/Research.js
@@ -1,22 +1,63 @@
 const express = require("express");
 const Research = require("../database/models/research");
 const User = require("../database/models/user");
-const {user_research} = require('../database/models/association/user_research')
+const {
+  user_research,
+} = require("../database/models/association/user_research");
 const router = express.Router();
 const { authentication } = require("../middlewares/authentication");
 router.use(authentication);
-
-
+const Branch = require("../database/models/branch");
+const { Op } = require("sequelize");
 
 router.get("/", async (request, response) => {
-  try{
+  try {
+    const { keyword, startYear, endYear, useYear, branchList, researchResult } =
+      request.query;
+    console.log(
+      keyword,
+      startYear,
+      endYear,
+      useYear,
+      branchList,
+      researchResult
+    );
+    const research = await Research.findAll({
+      include: [
+        {
+          model: User,
+          include: [
+            {
+              model: Branch,
+              where:
+                branchList?.length > 0
+                  ? {
+                      id: branchList || [],
+                    }
+                  : null,
+            },
+          ],
+        },
+      ],
+      where: {
+        [Op.and]: {
+          [Op.or]: {
+            researchNameTH: {
+              [Op.like]: `%${keyword}%`,
+            },
+          },
+          researchResult: {
+            [Op.like]: researchResult === "" ? "%%" : `%${researchResult}%`,
+          },
+        },
+      },
+    });
     return response.json({
       status: 200,
-      data: await Research.findAll({
-        include: User,
-      }),
+      data: research,
     });
-  }catch(e){
+  } catch (e) {
+    console.log(e);
     return response.status(500).json({
       error: "network error!",
     });
@@ -41,11 +82,10 @@ router.post("/", (request, response) => {
   }
 });
 
-
 router.get("/getResearchById/:id", async (request, response) => {
   try {
     const id = request.params.id;
-    if(id){
+    if (id) {
       const research = await Research.findAll({
         where: {
           researcherId: id,
@@ -56,7 +96,7 @@ router.get("/getResearchById/:id", async (request, response) => {
         data: research,
       });
     }
-    return response.status(200).json(null)
+    return response.status(200).json(null);
   } catch (e) {
     console.log(e);
     return response.status(500).json({
